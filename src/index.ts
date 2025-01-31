28f6f92ff9f7d4f640765550bb921a80c156ef05
Carlos V <carlosv.provoos@gmail.com>
2025-01-30 19:29:06 -0500

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -5,15 +5,13 @@ import {
     ImageDescriptionService,
 } from "./services/image";
 
-export type NodePlugin = ReturnType<typeof createNodePlugin>;
-
-export function createNodePlugin() {
-    return {
+const browserPlugin: Plugin = {
         name: "default",
         description: "Default plugin, with basic actions and evaluators",
         services: [
             new ImageDescriptionService(),
         ],
         actions: [describeImage],
-    } as const satisfies Plugin;
-}
+    } 
+
+export default browserPlugin;
